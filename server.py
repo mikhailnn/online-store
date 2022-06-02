3be838ca9a9f030cbf597e7f6e0cd8faad1c8d9d
mikhail <mn.02@mail.ru>
2022-06-02 21:53:29 +0300

    Модель каталога и вывод товаров на странице

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
+from catalog import catalog
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hello world!'
+    
+    return render_template('index.html', catalog=catalog)
 
 if __name__=="__main__":
     app.run(debug=True)
